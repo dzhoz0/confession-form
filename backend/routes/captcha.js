dac28262efeb7de52527bf972644b7177e502ad4
dzhoz0 <datuanho2k9@gmail.com>
2022-06-24 22:10:04 +0700

    Add base due to bug

--- backend/routes/captcha.js
+++ backend/routes/captcha.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const svgCaptcha = require("svg-captcha");
 const axios = require("axios");
 
-const api_base = `https://database.deta.sh/v1/${process.env.DETA_PROJECT_ID}/`;
+const api_base = `https://database.deta.sh/v1/${process.env.DETA_PROJECT_ID}/captcha`;
 const project_key = process.env.DETA_PROJECT_KEY;
 
 router.get("/captcha", async function (req, res, next) {
@@ -25,12 +25,14 @@ router.get("/captcha", async function (req, res, next) {
   };
 
   let response = await axios(api_base + "/items", config);
-  console.log(response.data);
+  //console.log(response.data);
 
-  let id = response.data.key;
-  res.send({
-    id: id,
-    captcha: captcha.data,
-  });
+  //let id = response.data.key;
+  //res.send({
+  //id: id,
+  //captcha: captcha.data,
+  //});
+  //
+  res.send(response);
 });
 module.exports = router;
